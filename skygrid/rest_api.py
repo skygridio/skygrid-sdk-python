b2906de22d932415925d284050f2c558c8ff7e9c
Jabez Wilson <jabez.wilson0@gmail.com>
2016-12-29 15:00:30 +1100

    fixed a syntax issue with id

--- skygrid/rest_api.py
+++ skygrid/rest_api.py
@@ -8,7 +8,11 @@ class RestApi(object):
     self._master_key = None
 
     self._endPoints = {
-      'getServerTime': get_server_time
+      'getServerTime': get_server_time,
+      'login':login,
+      'logout':logout,
+      'loginMaster':login_master,
+      'signup':signup
     }
 
   def _fetchJson(self,url,params):
@@ -26,7 +30,7 @@ class RestApi(object):
 
     fullUrl = self._address + url
     r = request(
-      params.get('method','GET').upper(),
+      params.get('method','').upper(),
       fullUrl,
       headers=params.get('headers',{}),
       data=params.get('body',{}))
@@ -41,9 +45,36 @@ class RestApi(object):
       raise Exception('API end point {} does not exist on the REST API'.format(name))
 
     return self._endPoints[name](self,data)
-
+    
+# endpoints
 def get_server_time(self,data):
   return self._fetchJson('/time', {'method':'get'})
+
+def signup(self,data):
+  return self._fetchJson('/users',{
+    'method':'post',
+    'body':data
+  })
+  
+def login(self,data):
+  response = self._fetchJson('/login',{
+    'method':'post',
+    'body':data
+  })
+  self._token = response.json().get('token',None)
+  return response
+
+def logout(self,data):
+  return self._fetchJson('/logout',{'method':'post'})
+
+def login_master(self,data):
+  self._master_key = data.get('masterKey','')
+  return {
+    # HACK: to not cause an error in parseJSON
+    'status_code':204
+  }
+
+# end endpoints
 
 def parseJSON(r):
   if(r.status_code != 204):
--- test.py
+++ test.py
@@ -6,6 +6,6 @@ def fn(changes,device):
   print("~~end of {} entry~~".format(device.name()))
 
 pr = skygrid.Project('RHlD5jC0',address="http://localhost:3081",api='socketio')
-print(pr.fetchServerTime())
+print(pr.signup('jonas@mailinator.com','1234'))
 pr = skygrid.Project('RHlD5jC0',address="http://localhost:3080",api='rest')
-print(pr.fetchServerTime())
+print(pr.signup('joas@mailinator.com','1234'))
